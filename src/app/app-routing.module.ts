de69ddaefdc6ddeb0c66bdf01a9dd82a0c4640a5
Richard.Liao <richard.liao@tpisoftware.com>
2022-05-31 17:37:00 +0800

    chore: 建立 sign-in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './feature/pages/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // 註冊頁面路由 http://localhost/sign-in
+  { path: 'sign-in', component: SignInComponent },
+  //沒定義的路由為 **
+  { path: '**', redirectTo: 'sign-in' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
