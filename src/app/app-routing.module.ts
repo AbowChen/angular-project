75b9401ef99682934d1ed251855191b843a8931b
Fred Chen <fred.chen@tpisoftware.com>
2022-06-07 00:04:45 +0800

    Update sign-in component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './feature/pages/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: '**', redirectTo: 'sign-in' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
